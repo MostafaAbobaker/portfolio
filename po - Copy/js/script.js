a4c8805ee32aabfe443f80fdee9933d0532d54e4
ashooo <48315673+ashooo@users.noreply.github.com>
2020-06-19 11:06:59 +0200

    Add files via upload

---
+++ po - Copy/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.toggle').click(function(){
+        $('.toggle').toggleClass('active')
+    }); });
+ 
+    function open_close(){
+        $('#menu').toggleClass('show');
+    }
